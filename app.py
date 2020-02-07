905f650de2501a2c30365e696e79897699c03650
JJackol <jacekczerwonka@gmail.com>
2020-02-08 00:22:10 +0100

    isome more jsonerror fun

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect,  json
+from flask_json import JsonError, json_response, FlaskJSON
 
 from flask_sqlalchemy import SQLAlchemy
 from count import count_values_in_multiple_str
@@ -6,13 +7,13 @@ from count import count_values_in_multiple_str
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
 db = SQLAlchemy(app)
+FlaskJSON(app)
 
 
 class JsonFile(db.Model):
+    """JSON file table"""
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(500), nullable=False)
-
-
 
     def __repr__(self):
         return '<JsonFile - id: %s>' % self.id
@@ -33,6 +34,10 @@ def hello_world():
                                nr_val=nrOfVal,
                                nr_files=len(files)
                                )
+
+@app.route('/raise_error')
+def raise_error():
+    return json_response(description='Example text.', code=123)
 
 
 def add_json(data=None):
