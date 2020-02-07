31928031eeede9372aba98a66d38c3fa8f5184fc
JJackol <jacekczerwonka@gmail.com>
2020-02-07 21:11:54 +0100

    db insert

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 to create db run those commands in virtualenv with requirements installed
 
 ```python
-$ python
->>> from app imort db
->>> db.create_all()
+from app import db
+db.create_all()
 ```
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, json
-# from models import db
+from flask import Flask, render_template, request, redirect,  json
+
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,29 +9,37 @@ db = SQLAlchemy(app)
 
 class JsonFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    json = db.Column(db.JSON, nullable=False)
+    data = db.Column(db.String(500), nullable=False)
+
+    def __init__(self, **kwargs):
+        super(JsonFile, self).__init__(**kwargs)
+        self.data = kwargs['data']
 
     def __repr__(self):
-        return '# JsonFile - id: %s' % self.id
+        return '<JsonFile - id: %s>' % self.id
 
 
 @app.route('/home', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'POST':
-
-        json_file = None
-        try:
-            db.session.add(json_file)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+        data = request.form['content']
+        return add_json(data)
 
     else:
-        return render_template('home.html')
-    return render_template("home.html")
+        files = [ f.data for f in JsonFile.query.all() ]
+        return render_template('home.html', files=files)
 
+
+def add_json(data=None):
+    try:
+        obj = json.loads(data, app)
+        json_file = JsonFile(data=json.dumps(obj, app))
+        db.session.add(json_file)
+        db.session.commit()
+        return redirect('/home')
+    except:
+        return 'There was a problem adding your json'
 
 if __name__ == '__main__':
     app.run(debug=True)
